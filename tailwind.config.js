8b1a7f6c9ba09cd4a1892936fc460749a42c94fa
Steven Sosnick <steven.sosnick@draftboard.co>
2023-11-03 11:14:06 -0400

    status

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["index.html"],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark: "#06070d",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
